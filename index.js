1f260b2346ac5a531f6bebafaff8405449c24733
Fran Leston <lestonramos@gmail.com>
2021-02-17 18:31:57 +0100

    ascenso

---
+++ index.js
@@ -0,0 +1,828 @@
+//Setup DB
+var jornada10 = [
+  {
+    numero: 1,
+    homePoints: 0,
+    homeTeam: "Celta B",
+    homeGoals: null,
+    awayTeam: "Guijuelo",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+];
+
+var jornada11 = [
+  {
+    numero: 2,
+    homePoints: 0,
+    homeTeam: "Racing",
+    homeGoals: null,
+    awayTeam: "Celta B",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+
+  {
+    numero: 3,
+    homePoints: 0,
+    homeTeam: "Numancia",
+    homeGoals: null,
+    awayTeam: "Cultural",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 4,
+    homePoints: 0,
+    homeTeam: "Guijuelo",
+    homeGoals: null,
+    awayTeam: "Zamora",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+];
+
+var jornada13 = [
+  {
+    numero: 5,
+    homePoints: 0,
+    homeTeam: "Lealtad",
+    homeGoals: null,
+    awayTeam: "Numancia",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+];
+
+var jornada14 = [
+  {
+    numero: 6,
+    homePoints: 0,
+    homeTeam: "Numancia",
+    homeGoals: null,
+    awayTeam: "Real Oviedo",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+];
+
+var jornada15 = [
+  {
+    numero: 7,
+    homePoints: 0,
+    homeTeam: "Sporting B",
+    homeGoals: null,
+    awayTeam: "Covadonga",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 8,
+    homePoints: 0,
+    homeTeam: "Langreo",
+    homeGoals: null,
+    awayTeam: "Marino",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 9,
+    homePoints: 0,
+    homeTeam: "Burgos",
+    homeGoals: null,
+    awayTeam: "Cultural",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 10,
+    homePoints: 0,
+    homeTeam: "Real Valladolid",
+    homeGoals: null,
+    awayTeam: "Numancia",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 11,
+    homePoints: 0,
+    homeTeam: "Real Oviedo",
+    homeGoals: null,
+    awayTeam: "Lealtad",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 12,
+    homePoints: 0,
+    homeTeam: "Unionistas",
+    homeGoals: null,
+    awayTeam: "Zamora",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 13,
+    homePoints: 0,
+    homeTeam: "Celta B",
+    homeGoals: null,
+    awayTeam: "Salamanca",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 14,
+    homePoints: 0,
+    homeTeam: "Pontevedra",
+    homeGoals: null,
+    awayTeam: "Compostela",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 15,
+    homePoints: 0,
+    homeTeam: "Racing",
+    homeGoals: null,
+    awayTeam: "Deportivo",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 16,
+    homePoints: 0,
+    homeTeam: "Guijuelo",
+    homeGoals: null,
+    awayTeam: "Coruxo",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+];
+
+var jornada16 = [
+  {
+    numero: 17,
+    homePoints: 0,
+    homeTeam: "Marino",
+    homeGoals: null,
+    awayTeam: "Covadonga",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 18,
+    homePoints: 0,
+    homeTeam: "Cultural",
+    homeGoals: null,
+    awayTeam: "Langreo",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 19,
+    homePoints: 0,
+    homeTeam: "Numancia",
+    homeGoals: null,
+    awayTeam: "Burgos",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 20,
+    homePoints: 0,
+    homeTeam: "Lealtad",
+    homeGoals: null,
+    awayTeam: "Real Valladolid",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 21,
+    homePoints: 0,
+    homeTeam: "Real Oviedo",
+    homeGoals: null,
+    awayTeam: "Sporting B",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 22,
+    homePoints: 0,
+    homeTeam: "Salamanca",
+    homeGoals: null,
+    awayTeam: "Zamora",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 23,
+    homePoints: 0,
+    homeTeam: "Compostela",
+    homeGoals: null,
+    awayTeam: "Celta B",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 24,
+    homePoints: 0,
+    homeTeam: "Deportivo",
+    homeGoals: null,
+    awayTeam: "Pontevedra",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 25,
+    homePoints: 0,
+    homeTeam: "Coruxo",
+    homeGoals: null,
+    awayTeam: "Racing",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 26,
+    homePoints: 0,
+    homeTeam: "Guijuelo",
+    homeGoals: null,
+    awayTeam: "Unionistas",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+];
+
+var jornada17 = [
+  {
+    numero: 27,
+    homePoints: 0,
+    homeTeam: "Marino",
+    homeGoals: null,
+    awayTeam: "Sporting B",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 28,
+    homePoints: 0,
+    homeTeam: "Covadonga",
+    homeGoals: null,
+    awayTeam: "Cultural",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 29,
+    homePoints: 0,
+    homeTeam: "Langreo",
+    homeGoals: null,
+    awayTeam: "Numancia",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 30,
+    homePoints: 0,
+    homeTeam: "Burgos",
+    homeGoals: null,
+    awayTeam: "Lealtad",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 31,
+    homePoints: 0,
+    homeTeam: "Real Valladolid",
+    homeGoals: null,
+    awayTeam: "Real Oviedo",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 32,
+    homePoints: 0,
+    homeTeam: "Salamanca",
+    homeGoals: null,
+    awayTeam: "Unionistas",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 33,
+    homePoints: 0,
+    homeTeam: "Zamora",
+    homeGoals: null,
+    awayTeam: "Compostela",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 34,
+    homePoints: 0,
+    homeTeam: "Celta B",
+    homeGoals: null,
+    awayTeam: "Deportivo",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 35,
+    homePoints: 0,
+    homeTeam: "Pontevedra",
+    homeGoals: null,
+    awayTeam: "Coruxo",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 36,
+    homePoints: 0,
+    homeTeam: "Racing",
+    homeGoals: null,
+    awayTeam: "Guijuelo",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+];
+
+var jornada18 = [
+  {
+    numero: 37,
+    homePoints: 0,
+    homeTeam: "Numancia",
+    homeGoals: null,
+    awayTeam: "Covadonga",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 38,
+    homePoints: 0,
+    homeTeam: "Lealtad",
+    homeGoals: null,
+    awayTeam: "Langreo",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 39,
+    homePoints: 0,
+    homeTeam: "Real Oviedo",
+    homeGoals: null,
+    awayTeam: "Burgos",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 40,
+    homePoints: 0,
+    homeTeam: "Sporting B",
+    homeGoals: null,
+    awayTeam: "Real Valladolid",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 41,
+    homePoints: 0,
+    homeTeam: "Cultural",
+    homeGoals: null,
+    awayTeam: "Marino",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 42,
+    homePoints: 0,
+    homeTeam: "Compostela",
+    homeGoals: null,
+    awayTeam: "Salamanca",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 43,
+    homePoints: 0,
+    homeTeam: "Deportivo",
+    homeGoals: null,
+    awayTeam: "Zamora",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 44,
+    homePoints: 0,
+    homeTeam: "Coruxo",
+    homeGoals: null,
+    awayTeam: "Celta B",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 45,
+    homePoints: 0,
+    homeTeam: "Guijuelo",
+    homeGoals: null,
+    awayTeam: "Pontevedra",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+  {
+    numero: 46,
+    homePoints: 0,
+    homeTeam: "Unionistas",
+    homeGoals: null,
+    awayTeam: "Racing",
+    awayGoals: null,
+    awayPoints: 0,
+    completed: null,
+  },
+];
+
+var jornadas = [
+  jornada10,
+  jornada11,
+  jornada13,
+  jornada14,
+  jornada15,
+  jornada16,
+  jornada17,
+  jornada18,
+];
+
+var aTeams = [
+  { name: "Unionistas", points: 26, originalPoints: 26, accPoints: 0 },
+  { name: "Zamora", points: 22, originalPoints: 22, accPoints: 0 },
+  { name: "Celta B", points: 21, originalPoints: 21, accPoints: 0 },
+  { name: "Compostela", points: 20, originalPoints: 20, accPoints: 0 },
+  { name: "Deportivo", points: 20, originalPoints: 20, accPoints: 0 },
+  { name: "Pontevedra", points: 18, originalPoints: 18, accPoints: 0 },
+  { name: "Racing", points: 17, originalPoints: 17, accPoints: 0 },
+  { name: "Coruxo", points: 14, originalPoints: 14, accPoints: 0 },
+  { name: "Salamanca", points: 11, originalPoints: 11, accPoints: 0 },
+  { name: "Guijuelo", points: 9, originalPoints: 9, accPoints: 0 },
+];
+
+var bTeams = [
+  { name: "Burgos", points: 27, originalPoints: 27, accPoints: 0 },
+  { name: "Cultural", points: 22, originalPoints: 22, accPoints: 0 },
+  { name: "Real Valladolid", points: 22, originalPoints: 22, accPoints: 0 },
+  { name: "Langreo", points: 20, originalPoints: 20, accPoints: 0 },
+  { name: "Lealtad", points: 18, originalPoints: 18, accPoints: 0 },
+  { name: "Marino", points: 17, originalPoints: 17, accPoints: 0 },
+  { name: "Numancia", points: 16, originalPoints: 16, accPoints: 0 },
+  { name: "Real Oviedo", points: 15, originalPoints: 15, accPoints: 0 },
+  { name: "Sporting B", points: 14, originalPoints: 14, accPoints: 0 },
+  { name: "Covadonga", points: 10, originalPoints: 10, accPoints: 0 },
+];
+
+var secondPhaseGroup = [];
+var groups2 = [secondPhaseGroup];
+var groups = [aTeams, bTeams];
+
+//Display a table for each jornada
+generateRanking();
+
+drawMatchTable();
+
+function drawMatchTable() {
+  var jornadasDiv = document.getElementById("jornadas");
+  jornadas.forEach((jornada, key) => {
+    //Make a Table
+    let table = document.createElement("TABLE");
+    let tbody = document.createElement("TBODY");
+    table.appendChild(tbody);
+    let thead = table.createTHead();
+    let row = thead.insertRow();
+    let th = document.createElement("th");
+    let text = document.createTextNode("Jornanda restante " + (key + 1));
+    th.appendChild(text);
+    row.appendChild(th);
+
+    generateMatchTable(table, jornada);
+    jornadasDiv.appendChild(table);
+  });
+}
+
+function generateMatchTable(table, data) {
+  for (var element of data) {
+    let row = table.insertRow();
+    for (key in element) {
+      if (key != "originalPoints") {
+        var cell = row.insertCell();
+      } else if (key != "accPoints") {
+        var cell = row.insertCell();
+      } else if (key != "homePoints") {
+        var cell = row.insertCell();
+      } else if (key != "awayPoints") {
+        var cell = row.insertCell();
+      } else if (key != "completed") {
+        var cell = row.insertCell();
+      }
+
+      if (key == "originalPoints") {
+        //
+      } else if (key == "accPoints") {
+        //
+      } else if (key == "homePoints") {
+        //
+      } else if (key == "awayPoints") {
+        //
+      } else if (key == "completed") {
+        //
+      } else if (key == "homeGoals") {
+        //make an input
+        var inputHomeGoals = document.createElement("input");
+        inputHomeGoals.type = "number";
+        inputHomeGoals.className = "match-" + element.numero;
+        inputHomeGoals.onchange = function (event) {
+          element.homeGoals = Number(event.target.value);
+          calculate();
+        };
+        cell.appendChild(inputHomeGoals);
+      } else if (key == "awayGoals") {
+        //make an input
+        var inputAwayGoals = document.createElement("input");
+        inputAwayGoals.type = "number";
+        inputAwayGoals.className = "match-" + element.numero;
+
+        inputAwayGoals.onchange = function (event) {
+          element.awayGoals = Number(event.target.value);
+          calculate();
+        };
+        cell.appendChild(inputAwayGoals);
+      } else {
+        if (element[key] != null) {
+          var text = document.createTextNode(element[key]);
+        } else {
+          var text = document.createTextNode("");
+        }
+        cell.appendChild(text);
+      }
+    }
+  }
+}
+
+function generateRanking() {
+  var groupsDiv = document.getElementById("groups");
+  while (groupsDiv.firstChild) {
+    groupsDiv.removeChild(groupsDiv.firstChild);
+  }
+  groups.forEach((group, key) => {
+    //Make a Table
+    let table = document.createElement("TABLE");
+    let tbody = document.createElement("TBODY");
+    table.appendChild(tbody);
+    let thead = table.createTHead();
+    let row = thead.insertRow();
+    let th = document.createElement("th");
+    let text = document.createTextNode("Grupo " + (key + 1));
+    th.appendChild(text);
+    row.appendChild(th);
+
+    generateMatchTable(table, group);
+
+    groupsDiv.appendChild(table);
+  });
+}
+
+function generateSecondRanking() {
+  var groupsDiv = document.getElementById("second-group");
+  while (groupsDiv.firstChild) {
+    groupsDiv.removeChild(groupsDiv.firstChild);
+  }
+  groups2.forEach((group, key) => {
+    //Make a Table
+    let table = document.createElement("TABLE");
+    let tbody = document.createElement("TBODY");
+    table.appendChild(tbody);
+    let thead = table.createTHead();
+    let row = thead.insertRow();
+    let th = document.createElement("th");
+    let text = document.createTextNode("Segunda Fase");
+    th.appendChild(text);
+    row.appendChild(th);
+
+    generateMatchTable(table, group);
+
+    groupsDiv.appendChild(table);
+  });
+}
+
+function updateData(match) {
+  var matchPointsHome, matchPointsAway;
+
+  if (match.homeGoals > match.awayGoals) {
+    if (match.completed == null) {
+      match.homePoints = match.homePoints + 3;
+      match.completed = 1;
+      matchPointsHome = 3;
+      matchPointsAway = 0;
+      updatePoints(match, matchPointsHome, matchPointsAway);
+    } else if (match.completed == 3) {
+      match.homePoints = match.homePoints + 2;
+      match.awayPoints = match.awayPoints - 1;
+      match.completed = 1;
+      matchPointsHome = 2;
+      matchPointsAway = -1;
+
+      updatePoints(match, matchPointsHome, matchPointsAway);
+    } else if (match.completed === 2) {
+      match.homePoints = match.homePoints + 3;
+      match.awayPoints = match.awayPoints - 3;
+      match.completed = 1;
+      matchPointsHome = 3;
+      matchPointsAway = -3;
+      updatePoints(match, matchPointsHome, matchPointsAway);
+    }
+  } else if (match.homeGoals < match.awayGoals) {
+    if (match.completed == null) {
+      match.awayPoints = match.awayPoints + 3;
+      match.completed = 2;
+      matchPointsHome = 0;
+      matchPointsAway = 3;
+      updatePoints(match, matchPointsHome, matchPointsAway);
+    } else if (match.completed == 3) {
+      match.homePoints = match.homePoints - 1;
+      match.awayPoints = match.awayPoints + 2;
+      match.completed = 2;
+      matchPointsHome = -1;
+      matchPointsAway = +2;
+      updatePoints(match, matchPointsHome, matchPointsAway);
+    } else if (match.completed === 1) {
+      match.homePoints = match.homePoints - 3;
+      match.awayPoints = match.awayPoints + 3;
+      match.completed = 2;
+      matchPointsHome = -3;
+      matchPointsAway = 3;
+      updatePoints(match, matchPointsHome, matchPointsAway);
+    }
+  } else if (match.homeGoals == match.awayGoals) {
+    if (match.completed == null) {
+      match.homePoints = match.homePoints + 1;
+      match.awayPoints = match.awayPoints + 1;
+      match.completed = 3;
+      matchPointsHome = 1;
+      matchPointsAway = 1;
+      updatePoints(match, matchPointsHome, matchPointsAway);
+    } else if (match.completed == 1) {
+      match.homePoints = match.homePoints - 2;
+      match.awayPoints = match.awayPoints + 1;
+      match.completed = 3;
+      matchPointsHome = -2;
+      matchPointsAway = 1;
+      updatePoints(match, matchPointsHome, matchPointsAway);
+    } else if (match.completed === 2) {
+      match.homePoints = match.homePoints + 1;
+      match.awayPoints = match.awayPoints - 2;
+      match.completed = 3;
+      matchPointsHome = 1;
+      matchPointsAway = -2;
+      updatePoints(match, matchPointsHome, matchPointsAway);
+    }
+  }
+}
+
+function updatePoints(match, matchPointsHome, matchPointsAway) {
+  var result = bTeams.filter((obj) => {
+    return obj.name === match.homeTeam;
+  });
+  if (result.length < 1) {
+    var result = aTeams.filter((obj) => {
+      return obj.name === match.homeTeam;
+    });
+  }
+
+  result[0].accPoints = result[0].accPoints + matchPointsHome;
+  result[0].points = result[0].originalPoints + result[0].accPoints;
+
+  var result2 = bTeams.filter((obj) => {
+    return obj.name === match.awayTeam;
+  });
+  if (result2.length < 1) {
+    var result2 = aTeams.filter((obj) => {
+      return obj.name === match.awayTeam;
+    });
+  }
+  result2[0].accPoints = result2[0].accPoints + matchPointsAway;
+  result2[0].points = result2[0].originalPoints + +result2[0].accPoints;
+
+  aTeams.sort(function (a, b) {
+    return b.points - a.points;
+  });
+  bTeams.sort(function (a, b) {
+    return b.points - a.points;
+  });
+
+  generateRanking();
+  makeSecondGroup();
+}
+
+var autoFillButton = document.getElementById("autoFill");
+autoFillButton.onclick = function () {
+  populate();
+};
+
+function populate() {
+  //update jornada
+
+  function weightedRandom(prob) {
+    let i,
+      sum = 0,
+      r = Math.random();
+    for (i in prob) {
+      sum += prob[i];
+      if (r <= sum) return i;
+    }
+  }
+  //get all inputs on the page
+  inputs = document.getElementsByTagName("input");
+  for (index = 0; index < inputs.length; ++index) {
+    var input = inputs[index];
+    var random = weightedRandom({
+      0: 0.3,
+      1: 0.24,
+      2: 0.2,
+      3: 0.15,
+      4: 0.08,
+      5: 0.03,
+    });
+
+    input.value = random;
+  }
+  calculate();
+}
+
+function calculate() {
+  jornadas.forEach((jornada) => {
+    for (let index = 0; index < jornada.length; index++) {
+      var match = jornada[index];
+      var inputs = document.getElementsByClassName("match-" + match.numero);
+      if (inputs[0].value != "" && inputs[1].value != "") {
+        match.homeGoals = Number(inputs[0].value);
+        match.awayGoals = Number(inputs[1].value);
+        updateData(match);
+      }
+    }
+  });
+}
+
+makeSecondGroup();
+function makeSecondGroup() {
+  secondPhaseGroup = aTeams.slice(0, 3).concat(bTeams.slice(0, 3));
+  secondPhaseGroup.sort(function (a, b) {
+    return b.points - a.points;
+  });
+
+  var depor = secondPhaseGroup.filter((obj) => {
+    return obj.name === "Deportivo";
+  });
+
+  groups2 = [secondPhaseGroup];
+  generateSecondRanking();
+}
